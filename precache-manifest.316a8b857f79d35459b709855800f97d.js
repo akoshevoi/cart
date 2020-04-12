0c9fccaad217ab3c4ce326bbb0cbbf523d6db8aa
akoshevoi <53705782+akoshevoi@users.noreply.github.com>
2020-04-12 19:28:51 +0300

    Updates

---
+++ precache-manifest.316a8b857f79d35459b709855800f97d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ebb8a58c04e4809be1fe31b7d61ac8b9",
+    "url": "/cart/index.html"
+  },
+  {
+    "revision": "6ba05902bc5df633741b",
+    "url": "/cart/static/css/main.d21b07ad.chunk.css"
+  },
+  {
+    "revision": "c5340d181d08268cc904",
+    "url": "/cart/static/js/2.dda3dce2.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/cart/static/js/2.dda3dce2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6ba05902bc5df633741b",
+    "url": "/cart/static/js/main.f0bb2fc1.chunk.js"
+  },
+  {
+    "revision": "85737296a4e8004ca842",
+    "url": "/cart/static/js/runtime-main.a5810a41.js"
+  }
+]);
